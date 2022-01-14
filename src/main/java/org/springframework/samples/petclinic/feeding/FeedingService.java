d6c080d49d9dbd8263a7d2b602835feb40b242cd
Gregorio <greortsol@google.com>
2022-01-14 10:01:53 +0100

    2-7 Working

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,22 +2,40 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedingService {
-    public List<Feeding> getAll(){
-        return null;
+
+    @Autowired
+    private FeedingRepository feedingRepository;
+
+    public List<Feeding> getAll() {
+        List<Feeding> f = feedingRepository.findAll();
+        return f;
     }
 
-    public List<FeedingType> getAllFeedingTypes(){
+    public List<FeedingType> getAllFeedingTypes() {
         return null;
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        FeedingType ft = feedingRepository.findFeedingType(typeName);
+        return ft;
     }
 
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+        Pet pet = p.getPet();
+        PetType pt = p.getFeedingType().getPetType();
+        if (pet.getType() != pt) {
+            throw new UnfeasibleFeedingException();
+        } else {
+            feedingRepository.save(p);
+        }
+        return p;
     }
 
-    
 }
